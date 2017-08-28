bfaa778142b83f417c578515707013594b4e1752
oharsta <oharsta@zilverline.com>
2017-08-28 13:06:11 +0200

    Do not show the username / password for push endpoint

--- mr-server/src/main/java/mr/conf/Push.java
+++ mr-server/src/main/java/mr/conf/Push.java
@@ -2,9 +2,13 @@ package mr.conf;
 
 import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
 public class Push {
 
     public final String url;
     public final String name;
+
+    public Push(String url, String name) {
+        this.url = url.replaceFirst("://(.*)@", "://");
+        this.name = name;
+    }
 }
