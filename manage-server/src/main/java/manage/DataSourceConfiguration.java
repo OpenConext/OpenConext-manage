00b4becf55e85a7f8e0e21106059d321a0c843b6
oharsta <oharsta@zilverline.com>
2017-11-24 10:51:38 +0100

    Ensure the pool can be created

--- manage-server/src/main/java/manage/DataSourceConfiguration.java
+++ manage-server/src/main/java/manage/DataSourceConfiguration.java
@@ -1,44 +1,64 @@
 package manage;
 
+import org.apache.tomcat.jdbc.pool.ConnectionPool;
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.util.Assert;
 
-import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 
 @Configuration
 public class DataSourceConfiguration {
 
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties srDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
     @Bean("dataSource")
     @Primary
     @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource srDataSource() {
-        DataSource dataSource = DataSourceBuilder.create().build();
-        if (dataSource instanceof org.apache.tomcat.jdbc.pool.DataSource) {
-            return tomcatDataSource(dataSource);
-        }
-        throw new IllegalArgumentException("Expected tomcat DataSource. Got " + dataSource.getClass().getName());
+    public DataSource srDataSource() throws SQLException {
+        DataSource dataSource = (DataSource) srDataSourceProperties().initializeDataSourceBuilder().type(DataSource.class)
+            .build();
+        setTypeSpecificProperties(dataSource);
+        return dataSource;
+
     }
 
-    private DataSource tomcatDataSource(DataSource dataSource) {
-        org.apache.tomcat.jdbc.pool.DataSource tomcatDataSource = (org.apache.tomcat.jdbc.pool.DataSource) dataSource;
-        tomcatDataSource.setTestOnBorrow(true);
-        tomcatDataSource.setValidationQuery("SELECT 1");
-        tomcatDataSource.setRemoveAbandoned(true);
-        tomcatDataSource.setTestWhileIdle(true);
-        tomcatDataSource.setLogValidationErrors(true);
-        return tomcatDataSource;
+    @Bean
+    @ConfigurationProperties("eb.datasource")
+    public DataSourceProperties ebDataSourceProperties() {
+        return new DataSourceProperties();
     }
 
     @Bean("ebDataSource")
     @ConfigurationProperties(prefix = "eb.datasource")
     public DataSource ebDataSource() {
-        DataSource dataSource = DataSourceBuilder.create().build();
-        if (dataSource instanceof org.apache.tomcat.jdbc.pool.DataSource) {
-            return tomcatDataSource(dataSource);
-        }
-        throw new IllegalArgumentException("Expected tomcat DataSource. Got " + dataSource.getClass().getName());
+        DataSource dataSource = (DataSource) ebDataSourceProperties().initializeDataSourceBuilder().type(DataSource.class)
+            .build();
+        setTypeSpecificProperties(dataSource);
+        return dataSource;
+    }
+
+    private void setTypeSpecificProperties(DataSource tomcatDataSource) {
+        tomcatDataSource.setTestOnBorrow(true);
+        tomcatDataSource.setValidationQuery("SELECT 1");
+        tomcatDataSource.setRemoveAbandoned(true);
+        tomcatDataSource.setTestWhileIdle(true);
+        tomcatDataSource.setLogValidationErrors(true);
+
+        ConnectionPool pool = tomcatDataSource.getPool();
+        Assert.notNull(pool, "Tomcat datasource pool is null");
+
     }
 }
