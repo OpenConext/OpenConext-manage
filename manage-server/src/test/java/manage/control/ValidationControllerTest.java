15950aa99c88708d0574f1910232a8e14e68fce6
oharsta <oharsta@zilverline.com>
2018-09-06 08:30:51 +0200

    Bugfix for to strict email validation

--- manage-server/src/test/java/manage/control/ValidationControllerTest.java
+++ manage-server/src/test/java/manage/control/ValidationControllerTest.java
@@ -33,8 +33,8 @@ public class ValidationControllerTest extends AbstractIntegrationTest {
 
     @Test
     public void validationEmail() throws Exception {
-        doValidation("email", "conext-beheer@surfnet.nl", true);
-        doValidation("email", "nope", false);
+        doValidation("local-email", "conext-beheer@surfnet.nl", true);
+        doValidation("local-email", "nope", false);
     }
 
     @Test
