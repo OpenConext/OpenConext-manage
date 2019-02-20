bd14c38c3785243757e2832926b5059fe6850065
Tim van Dijen <tvdijen@gmail.com>
2019-02-20 09:41:46 +0100

    Update email pattern
    Ampersands are perfectly legal in email addresses

--- manage-server/src/main/java/manage/validations/LocalEmailFormatValidator.java
+++ manage-server/src/main/java/manage/validations/LocalEmailFormatValidator.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 public class LocalEmailFormatValidator implements FormatValidator {
 
     private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
-        Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,16}$", Pattern.CASE_INSENSITIVE);
+        Pattern.compile("^[A-Z0-9._%&+-]+@[A-Z0-9.-]+\\.[A-Z]{2,16}$", Pattern.CASE_INSENSITIVE);
 
     @Override
     public Optional<String> validate(String subject) {
