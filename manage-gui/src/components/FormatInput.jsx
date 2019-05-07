0179894b6d41942cdfd0af72391d7755cd25063d
Malou <malou_allertz@hotmail.com>
2019-05-07 09:09:15 +0200

    secure entry for password input

--- manage-gui/src/components/FormatInput.jsx
+++ manage-gui/src/components/FormatInput.jsx
@@ -2,60 +2,72 @@ import React from "react";
 import PropTypes from "prop-types";
 import I18n from "i18n-js";
 
-import {validation} from "./../api";
-import {isEmpty} from "../utils/Utils";
+import { validation } from "./../api";
+import { isEmpty } from "../utils/Utils";
 
 import "./FormatInput.css";
 
 export default class FormatInput extends React.PureComponent {
-
     constructor(props) {
         super(props);
+
         this.state = {
             error: props.isError,
-            errorMessage: I18n.t("metaDataFields.error", {format: props.format})
+            errorMessage: I18n.t("metaDataFields.error", { format: props.format })
         };
     }
 
     componentDidMount () {
-        if (this.props.autofocus && this.input !== null) {
-            this.input.focus();
-        }
+        if (this.props.autofocus) this.input.focus();
     }
 
-    onChange = e => this.props.onChange(e.target.value);
-
-    onBlur = format => e => {
+    onBlur = e => {
         const value = e.target.value;
-        const {isRequired = true} = this.props;
-        if (isEmpty(value)) {
-            this.setState({
-                error: isRequired
-            });
-            this.props.onError(isRequired);
+        const { format, isRequired = true } = this.props;
 
+        if (isEmpty(value)) {
+            this.setState({ error: isRequired });
+            this.props.onError(isRequired);
         } else {
             validation(format, value).then(result => {
-                this.setState({
-                    error: !result
-                });
+                this.setState({ error: !result });
                 this.props.onError(!result);
             })
         }
     };
 
-    render() {
-        const {name, input, format, readOnly} = this.props;
-        const {error, errorMessage} = this.state;
-        const className = error ? "error" : "";
-        return <div className="format-input">
-            <input ref={ref => this.input = ref} className={className} type="text" id={name} name={name} value={input}
-                   onChange={this.onChange} onBlur={this.onBlur(format)} disabled={readOnly}/>
-            {error && <span><i className="fa fa-warning"></i>{errorMessage}</span>}
-        </div>
-
+    getInputType() {
+      switch (this.props.format) {
+        case 'password':
+          return 'password';
+        default:
+          return 'text'
+      }
     }
 
+    render() {
+        const { name, input, readOnly, onChange } = this.props;
+        const { error, errorMessage } = this.state;
+
+        return (
+            <div className="format-input">
+                <input
+                    ref={ref => this.input = ref}
+                    className={error && "error"}
+                    type={this.getInputType()}
+                    id={name}
+                    name={name}
+                    value={input}
+                    onChange={e => onChange(e.target.value)}
+                    onBlur={this.onBlur}
+                    disabled={readOnly}
+                />
+                { error &&
+                    <span><i className="fa fa-warning"></i>{errorMessage}</span>
+                }
+            </div>
+        )
+    }
 }
 
 FormatInput.propTypes = {
@@ -69,5 +81,3 @@ FormatInput.propTypes = {
     isRequired: PropTypes.bool,
     readOnly: PropTypes.bool
 };
-
-
