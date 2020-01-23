3c97a4e6a90384d30d12a8dce4863032762efb40
Okke Harsta <oharsta@zilverline.com>
2020-01-23 16:16:28 +0100

    Mark session cookies as secure

--- manage-server/src/main/java/manage/conf/SessionConfig.java
+++ manage-server/src/main/java/manage/conf/SessionConfig.java
@@ -1,14 +1,25 @@
 package manage.conf;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.mongo.JdkMongoSessionConverter;
 import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
 @EnableMongoHttpSession
 public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
+
+    @Bean
+    CookieSerializer cookieSerializer(@Value("${secure_cookie}") boolean secureCookie) {
+        DefaultCookieSerializer defaultCookieSerializer = new DefaultCookieSerializer();
+        defaultCookieSerializer.setUseSecureCookie(secureCookie);
+        return defaultCookieSerializer;
+    }
+
     @Bean
     public JdkMongoSessionConverter jdkMongoSessionConverter() {
         return new JdkMongoSessionConverter();
