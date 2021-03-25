90548d1cb4ea989fb27d2cbed4cb2bce5054a8ef
Stephan Kok <s.kok@kennisnet.nl>
2021-03-25 12:03:09 +0100

    Create test.properties to make the resources used during integration testing configurable

--- manage-server/src/test/java/manage/ApplicationTest.java
+++ manage-server/src/test/java/manage/ApplicationTest.java
@@ -2,13 +2,14 @@ package manage;
 
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static io.restassured.RestAssured.given;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.hamcrest.Matchers.equalTo;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        properties = {"spring.profiles.active=dev", "spring.data.mongodb.uri=mongodb://localhost:27017/metadata_test"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations= "classpath:test.properties")
 public class ApplicationTest extends AbstractIntegrationTest {
 
     @Test
