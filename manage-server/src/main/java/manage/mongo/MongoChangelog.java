cc8aa39d30e138d761f73ba9a2e0794e13406aa2
Okke Harsta <oharsta@zilverline.com>
2022-03-07 15:00:48 +0100

    Bugfix for not creating change requests collections

--- manage-server/src/main/java/manage/mongo/MongoChangelog.java
+++ manage-server/src/main/java/manage/mongo/MongoChangelog.java
@@ -185,7 +185,7 @@ public class MongoChangelog {
     public void createChangeRequestsCollections(MongockTemplate mongoTemplate) {
         Stream.of(EntityType.values()).forEach(entityType -> {
             String revisionCollection = entityType.getType().concat(REVISION_POSTFIX);
-            if (!mongoTemplate.collectionExists(entityType.getType())) {
+            if (!mongoTemplate.collectionExists(revisionCollection)) {
                 mongoTemplate.createCollection(revisionCollection);
             }
         });
