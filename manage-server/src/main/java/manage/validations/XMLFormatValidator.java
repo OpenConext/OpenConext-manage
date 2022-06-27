71750125d73044b8e57e4f7a284c84bcc440dc9a
Vincent Jong <jong.vincent@kpmg.nl>
2022-06-27 15:39:06 +0200

    Resolve XXE vulnerability in XML validation

--- manage-server/src/main/java/manage/validations/XMLFormatValidator.java
+++ manage-server/src/main/java/manage/validations/XMLFormatValidator.java
@@ -3,6 +3,7 @@ package manage.validations;
 import org.everit.json.schema.FormatValidator;
 import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
@@ -11,12 +12,16 @@ import java.util.Optional;
 
 public class XMLFormatValidator implements FormatValidator {
 
+    private static final String FEATURE_DISABLE_DOCTYPE = "http://apache.org/xml/features/disallow-doctype-decl";
+
     @Override
     public Optional<String> validate(String subject) {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         factory.setValidating(false);
         factory.setNamespaceAware(true);
         try {
+            factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            factory.setFeature(FEATURE_DISABLE_DOCTYPE, true);
             factory.newDocumentBuilder().parse(new ByteArrayInputStream(subject.getBytes()));
         } catch (ParserConfigurationException | SAXException | IOException e) {
             return Optional.of(e.toString());
