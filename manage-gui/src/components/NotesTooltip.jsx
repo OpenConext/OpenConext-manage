4c8c8c9e7893a07fdd000438c7559922c4bf38c5
Okke Harsta <oharsta@zilverline.com>
2022-08-09 15:21:56 +0200

    Style notes tooltip

--- manage-gui/src/components/NotesTooltip.jsx
+++ manage-gui/src/components/NotesTooltip.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ReactTooltip from "react-tooltip";
+import "./NotesTooltip.scss";
 
 export default function NotesTooltip({identifier, notes}) {
   return (
-    <span data-for={identifier} data-tip>
+    <span className={"notes-tooltip"} data-for={identifier} data-tip>
             <i className="fa fa-info-circle"></i>
             <ReactTooltip id={identifier} type="info" class="tool-tip" effect="solid">
                 <span>{notes}</span>
