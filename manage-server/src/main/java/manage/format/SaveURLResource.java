b450ab805a5e51add0df39c938131549a900ca6a
Vincent Jong <jong.vincent@kpmg.nl>
2023-12-19 12:09:37 +0100

    Add configurable user agent for retrieving metadata

--- manage-server/src/main/java/manage/format/SaveURLResource.java
+++ manage-server/src/main/java/manage/format/SaveURLResource.java
@@ -2,19 +2,34 @@ package manage.format;
 
 import org.springframework.core.io.UrlResource;
 
+import java.io.IOException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Arrays;
 import java.util.List;
 
 public class SaveURLResource extends UrlResource {
 
+    private static final String USER_AGENT_KEY = "User-Agent";
+
     private final List<String> allowedProtocols = Arrays.asList("http", "https");
 
-    public SaveURLResource(URL url, boolean dev) {
+    private final String userAgent;
+
+    public SaveURLResource(URL url, boolean dev, String userAgent) {
         super(url);
+        this.userAgent = userAgent;
         String protocol = url.getProtocol();
         if (!dev && !allowedProtocols.contains(protocol)) {
             throw new IllegalArgumentException(String.format("Not allowed protocol %s - allowed protocols are %s", protocol, allowedProtocols));
+        }
+    }
+
+    @Override
+    protected void customizeConnection(URLConnection con) throws IOException {
+        super.customizeConnection(con);
+        if (null != userAgent && !userAgent.isEmpty()) {
+            con.setRequestProperty(USER_AGENT_KEY, userAgent);
         }
     }
 }
--- manage-server/src/test/java/manage/format/SaveURLResourceTest.java
+++ manage-server/src/test/java/manage/format/SaveURLResourceTest.java
@@ -1,19 +1,38 @@
 package manage.format;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
 public class SaveURLResourceTest {
+
+    @Mock
+    URLConnection connection;
 
     @Test(expected = IllegalArgumentException.class)
     public void testNotAllowedProtocol() throws MalformedURLException {
-        SaveURLResource resource = new SaveURLResource(new URL("file://local"), false);
+        SaveURLResource resource = new SaveURLResource(new URL("file://local"), false, null);
     }
 
     @Test
     public void testNotAllowedProtocolInDev() throws MalformedURLException {
-        new SaveURLResource(new URL("file://local"), true);
+        new SaveURLResource(new URL("file://local"), true, null);
+    }
+
+    @Test
+    public void testUseUserAgent() throws IOException {
+        SaveURLResource resource = new SaveURLResource(new URL("https://external"), false, "user agent");
+        resource.customizeConnection(connection);
+        verify(connection, times(1)).setRequestProperty("User-Agent", "user agent");
     }
 }
