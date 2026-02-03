ac6dcf6e7d6cde46130a761d12a7d9e1a31eb117
Leroy <3416288+Liemine@users.noreply.github.com>
2026-02-03 13:17:20 +0100

    #604 Remove cra and add vite. Move robot from code to npm

--- manage-gui/src/main.jsx
+++ manage-gui/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import App from "./pages/App";
 import {createRoot} from 'react-dom/client';
+import "typeface-roboto";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 const container = document.getElementById('app');
