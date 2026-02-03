7d0975fa33672fea06e10d9437a6ab9a01d2a2cb
Leroy <3416288+Liemine@users.noreply.github.com>
2026-02-03 13:14:27 +0100

    #604 Remove cra and add vite. Move font-awesome from code to npm

--- manage-gui/src/main.jsx
+++ manage-gui/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import App from "./pages/App";
 import {createRoot} from 'react-dom/client';
+import "@fortawesome/fontawesome-free/css/all.css";
 
 const container = document.getElementById('app');
 const root = createRoot(container);
