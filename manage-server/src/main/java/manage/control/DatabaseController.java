c4f9101bc3794b16b8308cb754fc6d2ee97c0b9e
Okke Harsta <oharsta@zilverline.com>
2026-05-06 05:27:23 +0200

    Bugfix for wrong step(up) status in the backend

--- manage-server/src/main/java/manage/control/DatabaseController.java
+++ manage-server/src/main/java/manage/control/DatabaseController.java
@@ -234,7 +234,7 @@ public class DatabaseController {
                 return new ResponseEntity<>(Map.of("message", message), HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } else {
-            result.put("step", Map.of("status", "OK"));
+            result.put("stepup", Map.of("status", "OK"));
         }
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
