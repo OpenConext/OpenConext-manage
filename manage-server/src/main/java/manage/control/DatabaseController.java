2f762185e22617c40cc6160052b20b826b0d7dd3
Okke Harsta <oharsta@gmail.com>
2026-07-02 13:11:39 +0200

    Fixed build

--- manage-server/src/main/java/manage/control/DatabaseController.java
+++ manage-server/src/main/java/manage/control/DatabaseController.java
@@ -124,11 +124,11 @@ public class DatabaseController {
     }
 
     public ResponseEntity<Map> doPush(PushOptions pushOptions) {
-        if (environment.acceptsProfiles(Profiles.of("dev")) &&
-            !environment.acceptsProfiles(Profiles.of("integration-test"))) {
-        LOG.debug("Pushing with options {}", pushOptions);
 
-        if (environment.acceptsProfiles(Profiles.of("dev"))) {
+        LOG.debug("Pushing with options {} and environment {}", pushOptions, environment.getActiveProfiles());
+
+        if (environment.acceptsProfiles(Profiles.of("dev"))
+            && !environment.acceptsProfiles(Profiles.of("integration-test"))) {
             return new ResponseEntity<>(Map.of(
                 "eb", Map.of("status", "OK"),
                 "pdp", Map.of("status", "OK"),
